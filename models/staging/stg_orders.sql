4e7e509173d82e4ac4ec5fb3b463bd81736ec8f2
Wim De Bruycker <wim.debruycker@vlaanderen.be>
2023-01-30 11:28:11 +0000

    added delivery_teams seed

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -1,6 +1,7 @@
 select
     -- from raw_orders
-     o.orderid
+    {{ dbt_utils.generate_surrogate_key(['o.orderid', 'c.customerid', 'p.productid']) }} as sk_orders
+    ,o.orderid
     ,o.orderdate
     ,o.shipdate
     ,o.shipmode
@@ -18,11 +19,14 @@ select
     ,p.productname
     ,p.subcategory
     ,{{ markup('o.ordersellingprice','o.ordercostprice') }} as markup
+    -- from deliver_team
+    ,d.delivery_team
 from
    {{ ref('raw_orders') }} as o
 left join {{ ref('raw_customer') }} as c
     on o.customerid=c.customerid
 left join {{ ref('raw_product') }} as p
     on o.productid=p.productid
-
+left join {{ ref('delivery_team') }} as d
+    on o.shipmode=d.shipmode
 
